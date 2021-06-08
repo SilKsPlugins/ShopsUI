a40162bfe03ef57ab0b4701b442c4ae221d03623
IAmSilK <iamsilk3495@gmail.com>
2021-06-08 10:17:36 -0300

    Added admin item state

---
+++ ShopsUI - OpenMod/AdminItemState.cs
@@ -0,0 +1,23 @@
+﻿using OpenMod.Extensions.Games.Abstractions.Items;
+using OpenMod.Unturned.Items;
+
+namespace ShopsUI
+{
+    public class AdminItemState : IItemState
+    {
+        public AdminItemState(UnturnedItemAsset itemAsset)
+        {
+            ItemQuality = itemAsset.MaxQuality ?? 100;
+            ItemDurability = itemAsset.MaxDurability ?? 100;
+            ItemAmount = itemAsset.MaxAmount ?? 1;
+        }
+
+        public double ItemQuality { get; }
+
+        public double ItemDurability { get; }
+
+        public double ItemAmount { get; }
+
+        public byte[]? StateData { get; }
+    }
+}
