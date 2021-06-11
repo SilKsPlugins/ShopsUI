5e09d155abfeaad7b7710440f2849ef1ac23ed27
IAmSilK <iamsilk3495@gmail.com>
2021-06-11 15:13:48 -0300

    Added whitelist/blacklist group models for databases

---
+++ ShopsUI - OpenMod/Database/Models/ItemGroupModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopsUI.Database.Models
+{
+    [Table("ItemGroup")]
+    public class ItemGroupModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Permission { get; set; }
+
+        public bool IsWhitelist { get; set; }
+
+        public virtual ItemShopModel ItemShop { get; }
+
+        public ItemGroupModel()
+        {
+            Id = 0;
+            Permission = "";
+            IsWhitelist = true;
+            ItemShop = null!;
+        }
+    }
+}
---
+++ ShopsUI - OpenMod/Database/Models/VehicleGroupModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopsUI.Database.Models
+{
+    [Table("VehicleGroup")]
+    public class VehicleGroupModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Permission { get; set; }
+
+        public bool IsWhitelist { get; set; }
+
+        public virtual VehicleShopModel VehicleShop { get; }
+
+        public VehicleGroupModel()
+        {
+            Id = 0;
+            Permission = "";
+            IsWhitelist = true;
+            VehicleShop = null!;
+        }
+    }
+}
