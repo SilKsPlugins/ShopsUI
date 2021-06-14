718b5b411eb7444c32796560e5cee394c8a59e70
Stephen White <iamsilk3495@gmail.com>
2021-06-14 14:32:28 -0300

    Add /shop migrate command

---
+++ ShopsUI - OpenMod/Commands/CShopMigrate.cs
@@ -0,0 +1,227 @@
+﻿using Cysharp.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using OpenMod.API.Commands;
+using OpenMod.API.Prioritization;
+using OpenMod.Core.Commands;
+using OpenMod.Unturned.Commands;
+using SDG.Unturned;
+using ShopsUI.Commands.Items;
+using ShopsUI.Database;
+using ShopsUI.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+// ReSharper disable StringLiteralTypo
+// ReSharper disable IdentifierTypo
+
+namespace ShopsUI.Commands
+{
+    [Command("migrate", Priority = Priority.High)]
+    [CommandDescription("Migrates ZaupShop shops to ShopsUI.")]
+    [CommandParent(typeof(CShop))]
+    public class CShopMigrate : UnturnedCommand
+    {
+        // /Rocket/Plugins/Uconomy/Uconomy.configuration.xml
+        [Serializable]
+        public class UconomyConfiguration
+        {
+            public string? DatabaseAddress { get; set; }
+
+            public string? DatabaseUsername { get; set; }
+
+            public string? DatabasePassword { get; set; }
+
+            public string? DatabaseName { get; set; }
+
+            public int DatabasePort { get; set; }
+        }
+
+        // /Rocket/Plugins/ZaupShop/ZaupShop.configuration.xml
+        [Serializable]
+        public class ZaupShopConfiguration
+        {
+            public string ItemShopTableName { get; set; } = "uconomyitemshop";
+
+            public string VehicleShopTableName { get; set; } = "uconomyvehicleshop";
+
+            public string GroupListTableName { get; set; } = "zaupshopgroups";
+        }
+
+        private readonly ShopsDbContext _dbContext;
+
+        public CShopMigrate(ShopsDbContext dbContext,
+            IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            _dbContext = dbContext;
+        }
+
+        private async Task<T> GetPluginConfig<T>(string path) where T : class, new()
+        {
+            var fullPath = ReadWrite.PATH + ServerSavedata.transformPath(path);
+
+            if (!File.Exists(fullPath))
+            {
+                throw new UserFriendlyException(
+                    $"Could not find a plugin configuration file: {Path.GetFileName(path)}");
+            }
+            
+            using var streamReader = new StreamReader(fullPath);
+
+            var contents = await streamReader.ReadToEndAsync();
+
+            var reader = new StringReader(contents);
+
+            var serializer = new XmlSerializer(typeof(T));
+
+            try
+            {
+                return (T)serializer.Deserialize(reader);
+            }
+            catch (Exception)
+            {
+                throw new UserFriendlyException(
+                    $"There was an issue parsing the configuration file: {Path.GetFileName(path)}");
+            }
+        }
+
+        private async Task MigrateItemShops(MySqlConnection connection, ZaupShopConfiguration config)
+        {
+            var query = $"SELECT `id`,`cost`,`buyback` FROM `{config.ItemShopTableName}`;";
+
+            var command = new MySqlCommand(query, connection);
+
+            using var reader = command.ExecuteReader();
+
+            while (await reader.ReadAsync())
+            {
+                var id = reader.GetUInt16(0);
+                var buyPrice = reader.GetDecimal(1);
+                var sellPrice = reader.GetDecimal(2);
+
+                await _dbContext.ItemShops.AddAsync(new ItemShopModel
+                {
+                    ItemId = id,
+                    BuyPrice = buyPrice <= 0 ? null : buyPrice,
+                    SellPrice = sellPrice <= 0 ? null : sellPrice,
+                    Order = 0
+                });
+            }
+        }
+
+        private async Task MigrateVehicleShops(MySqlConnection connection, ZaupShopConfiguration config)
+        {
+            var query = $"SELECT `id`,`cost` FROM `{config.VehicleShopTableName}`;";
+
+            var command = new MySqlCommand(query, connection);
+
+            using var reader = command.ExecuteReader();
+
+            while (await reader.ReadAsync())
+            {
+                var id = reader.GetUInt16(0);
+                var buyPrice = reader.GetDecimal(1);
+
+                await _dbContext.VehicleShops.AddAsync(new VehicleShopModel
+                {
+                    VehicleId = id,
+                    BuyPrice = buyPrice,
+                    Order = 0
+                });
+            }
+        }
+
+        private async Task MigrateGroups(MySqlConnection connection, ZaupShopConfiguration config)
+        {
+            var mainGroupQuery = $"SELECT `name`,`whitelist` FROM `{config.GroupListTableName}`;";
+
+            var tables = new List<(string Name, bool Whitelist)>();
+
+            using (var command = new MySqlCommand(mainGroupQuery, connection))
+            {
+                using var reader = command.ExecuteReader();
+
+                while (await reader.ReadAsync())
+                {
+                    var name = reader.GetString(0);
+                    var whitelist = reader.GetByte(1) == 1; // 1 = whitelist, 0 = blacklist
+
+                    tables.Add((name, whitelist));
+                }
+            }
+
+
+            foreach (var (name, whitelist) in tables)
+            {
+                var groupTableQuery = $"SELECT `assetid`,`vehicle` FROM `{name}`;";
+
+                using var command = new MySqlCommand(groupTableQuery, connection);
+
+                using var reader = command.ExecuteReader();
+
+                while (await reader.ReadAsync())
+                {
+                    var id = reader.GetUInt16(0);
+                    var isVehicle = reader.GetByte(1) == 1; // 1 = vehicle, 0 = item
+
+                    if (isVehicle)
+                    {
+                        await _dbContext.VehicleGroups.AddAsync(new VehicleGroupModel
+                        {
+                            Permission = name,
+                            IsWhitelist = whitelist,
+                            VehicleShopVehicleId = id
+                        });
+                    }
+                    else
+                    {
+                        await _dbContext.ItemGroups.AddAsync(new ItemGroupModel
+                        {
+                            Permission = name,
+                            IsWhitelist = whitelist,
+                            ItemShopItemId = id
+                        });
+                    }
+                }
+            }
+        }
+
+        protected override async UniTask OnExecuteAsync()
+        {
+            await UniTask.SwitchToThreadPool();
+
+            await PrintAsync("Beginning shop migration. This may take a while.");
+
+            var uconomyConfig =
+                await GetPluginConfig<UconomyConfiguration>("/Rocket/Plugins/Uconomy/Uconomy.configuration.xml");
+            var zaupShopConfig =
+                await GetPluginConfig<ZaupShopConfiguration>("/Rocket/Plugins/ZaupShop/ZaupShop.configuration.xml");
+
+            var connectionString =
+                $"Server={uconomyConfig.DatabaseAddress};" +
+                $"Port={(uconomyConfig.DatabasePort == 0 ? 3306 : uconomyConfig.DatabasePort)};" +
+                $"Database={uconomyConfig.DatabaseName};" +
+                $"User={uconomyConfig.DatabaseUsername};" +
+                $"Password={uconomyConfig.DatabasePassword}";
+
+            using var connection = new MySqlConnection(connectionString);
+
+            await connection.OpenAsync();
+
+            await MigrateItemShops(connection, zaupShopConfig);
+
+            await MigrateVehicleShops(connection, zaupShopConfig);
+
+            await MigrateGroups(connection, zaupShopConfig);
+
+            await connection.CloseAsync();
+
+            var entries = await _dbContext.SaveChangesAsync();
+
+            await PrintAsync($"Successfully migrated and written {entries} new entries to the database.");
+            await PrintAsync("Make sure to replace your zaupgroup.XXX permissions with ShopsUI:groups.XXX");
+        }
+    }
+}
