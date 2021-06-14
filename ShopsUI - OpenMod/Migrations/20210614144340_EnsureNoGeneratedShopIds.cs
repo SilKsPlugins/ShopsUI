f5a43358885eadde7b8c7618d9077b4d340110ac
Stephen White <iamsilk3495@gmail.com>
2021-06-14 11:44:32 -0300

    Add migration to ensure no generated shop ids

---
+++ ShopsUI - OpenMod/Migrations/20210614144340_EnsureNoGeneratedShopIds.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopsUI.Migrations
+{
+    public partial class EnsureNoGeneratedShopIds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<ushort>(
+                name: "VehicleId",
+                table: "ShopsUI_VehicleShops",
+                nullable: false,
+                oldClrType: typeof(ushort),
+                oldType: "smallint unsigned")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<ushort>(
+                name: "ItemId",
+                table: "ShopsUI_ItemShops",
+                nullable: false,
+                oldClrType: typeof(ushort),
+                oldType: "smallint unsigned")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<ushort>(
+                name: "VehicleId",
+                table: "ShopsUI_VehicleShops",
+                type: "smallint unsigned",
+                nullable: false,
+                oldClrType: typeof(ushort))
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<ushort>(
+                name: "ItemId",
+                table: "ShopsUI_ItemShops",
+                type: "smallint unsigned",
+                nullable: false,
+                oldClrType: typeof(ushort))
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
