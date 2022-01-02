b35d0d0f1c4ef8f6aef8578469fd98b0a92f9d09
Stephen White <iamsilk3495@gmail.com>
2022-01-02 01:46:26 -0400

    Update default ShopsUI effect IDs to match newly uploaded workshop effects

--- ShopsUI - OpenMod/Configuration/UIConfig.cs
+++ ShopsUI - OpenMod/Configuration/UIConfig.cs
@@ -7,9 +7,9 @@ namespace ShopsUI.Configuration
     {
         public string? LogoUrl { get; set; } = "https://i.imgur.com/t6HbFTN.png";
 
-        public ushort ShopsEffect { get; set; } = 29150;
+        public ushort ShopsEffect { get; set; } = 29300;
 
-        public ushort SellBoxEffect { get; set; } = 29200;
+        public ushort SellBoxEffect { get; set; } = 29400;
 
         public bool BackgroundsEnabled { get; set; } = true;
 
