7029f490f9388109765f2a80923002db4c19fba4
Stephen White <iamsilk3495@gmail.com>
2022-01-17 21:29:49 -0400

    Fix /shop reload not reloading shops cache

--- ShopsUI - OpenMod/Commands/CShopReload.cs
+++ ShopsUI - OpenMod/Commands/CShopReload.cs
@@ -1,12 +1,12 @@
-﻿using Cysharp.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Cysharp.Threading.Tasks;
 using Microsoft.Extensions.Localization;
 using OpenMod.API.Prioritization;
 using OpenMod.Core.Commands;
 using OpenMod.Unturned.Commands;
+using ShopsUI.API.Shops.Items;
+using ShopsUI.API.Shops.Vehicles;
 using ShopsUI.Commands.Items;
-using ShopsUI.Database;
-using System;
 
 namespace ShopsUI.Commands
 {
@@ -15,21 +15,24 @@ namespace ShopsUI.Commands
     [CommandParent(typeof(CShop))]
     public class CShopReload : UnturnedCommand
     {
-        private readonly ShopsDbContext _dbContext;
+        private readonly IItemShopDirectory _itemShopDirectory;
+        private readonly IVehicleShopDirectory _vehicleShopDirectory;
         private readonly IStringLocalizer _stringLocalizer;
 
-        public CShopReload(ShopsDbContext dbContext,
-            IStringLocalizer stringLocalizer,
-            IServiceProvider serviceProvider) : base(serviceProvider)
+        public CShopReload(IServiceProvider serviceProvider,
+            IItemShopDirectory itemShopDirectory,
+            IVehicleShopDirectory vehicleShopDirectory,
+            IStringLocalizer stringLocalizer) : base(serviceProvider)
         {
-            _dbContext = dbContext;
+            _itemShopDirectory = itemShopDirectory;
+            _vehicleShopDirectory = vehicleShopDirectory;
             _stringLocalizer = stringLocalizer;
         }
 
         protected override async UniTask OnExecuteAsync()
         {
-            await _dbContext.ItemShops.LoadAsync();
-            await _dbContext.VehicleShops.LoadAsync();
+            await _itemShopDirectory.Invalidate(true);
+            await _vehicleShopDirectory.Invalidate(true);
 
             await PrintAsync(_stringLocalizer["commands:success:shop_reloaded"]);
         }
